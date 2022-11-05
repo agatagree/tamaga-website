0b0218f16d903b82869b534cf7e6a2cc07692e3d
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2022-11-05 21:54:35 +0100

    add carousel gallery home page

--- tamaga/src/utils/Card/Card.js
+++ tamaga/src/utils/Card/Card.js
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Card = ({ singleProject }) => {
-  console.log(`${singleProject.id}`)
   return (
     <div className="card-container">
       <Link to={`/projects/${singleProject.id}`}>
         <div className="card-overlay">
           <h4 className="H04">{singleProject.title}</h4>
-          <h7 className="H07">{singleProject.category}</h7>
+          <h6 className="H07">{singleProject.category}</h6>
         </div>
         <img
           className="card-img"
